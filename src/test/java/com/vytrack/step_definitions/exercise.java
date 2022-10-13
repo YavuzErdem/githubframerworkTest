1a4f4251522daa45d6b2e5fdb6e9a3322b71dfb9
YavuzErdem <yvzerdem06@gmail.com>
2022-10-13 01:17:19 +0100

    4 commit

--- src/test/java/com/vytrack/step_definitions/exercise.java
+++ src/test/java/com/vytrack/step_definitions/exercise.java
@@ -3,5 +3,9 @@ package com.vytrack.step_definitions;
 public class exercise {
     public static void main(String[] args) {
         System.out.println("yavuz");
+
+        System.out.println("afsasf");
+
+        System.out.println("safdaf");
     }
 }
